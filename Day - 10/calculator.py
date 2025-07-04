2d27eaee8b7c7ed05ad4344dd09923d552002a74
Rahul Singh <docoderahul@gmail.com>
2025-07-04 11:07:15 +0530

    Calculated Day 10 completed

--- Day - 10/calculator.py
+++ Day - 10/calculator.py
@@ -16,29 +16,41 @@ logo = '''
 '''
 symbol = ["+", "-","*","/"]
 
-first_number = int(input("Enter the first number? :"))
-for sym in symbol:
-    print("sym \n")
-final = 0
-try_again = True
-while try_again:
+first_try = True
+
+while first_try:
+    first_number = float(input("Enter the first number? :"))
+    for sym in symbol:
+        print(sym)
+    final = 0
+    try_again = True
+    while try_again:
     
-    operation = input("Pick an operation : ")
-    if operation in symbol:
-        second_number = int(input("What's the second number? :"))
-        if operation == "+":
-            final = first_number + second_number
-        elif operation == "-":
-            final = first_number - second_number
-        elif operation == "*":
-            final = first_number * second_number
-        elif operation == "/":
-            if second_number != 0:
-                final = first_number / second_number
-            else:
-                print("Cannot divide by zero.")
-
-
-        print(f"{first_number}{operation}{second_number} = ")
-    else:
-        print("Please Enter the valid symbols. !")
+        operation = input("Pick an operation : ")
+        if operation in symbol:
+            second_number = float(input("What's the second number? :"))
+            if operation == "+":
+                final = first_number + second_number
+            elif operation == "-":
+                final = first_number - second_number
+            elif operation == "*":
+                final = first_number * second_number
+            elif operation == "/":
+                if second_number != 0:
+                    final = first_number / second_number
+                else:
+                    print("Cannot divide by zero.")
+            print(f"{first_number}{operation}{second_number} = {final}")
+        else:
+            print("Please Enter the valid symbols. !")
+        more = input(f"Type 'y'to continue calculating with {final}, or type 'n' to start a new  calculation: ")
+        if more in ["y", "n"]:
+            if more == "y":
+                first_number = final
+            elif more =="n":
+                try_again =False
+                first_try =False
+                print(f"The Final answer of the calculation is {final}")
+                print("\n"*20)
+        else:
+            print("Please enter the valid keyword.!")
